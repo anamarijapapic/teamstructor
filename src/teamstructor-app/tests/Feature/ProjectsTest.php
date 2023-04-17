4c0100300c5a2bc937257a8ecf2b045d7714052f
Anamarija Papic <anamarija.papic@agilo.co>
2023-04-17 13:54:43 +0200

    Projects - feature testing

---
+++ src/teamstructor-app/tests/Feature/ProjectsTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Livewire\Projects\Projects;
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Livewire\Livewire;
+use Tests\TestCase;
+
+class ProjectsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_the_component_can_render(): void
+    {
+        $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+
+        $component = Livewire::test(Projects::class, ['team' => $user->fresh()->personalTeam()->id]);
+
+        $component->assertStatus(200);
+    }
+
+    public function test_projects_page_contains_livewire_component(): void
+    {
+        $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+
+        $this->get('/teams/'.$user->fresh()->personalTeam()->id.'/projects')->assertSeeLivewire(Projects::class);
+    }
+
+    public function test_projects_can_be_created(): void
+    {
+        $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+
+        Livewire::test(Projects::class, ['team' => $user->fresh()->personalTeam()->id])
+            ->set([
+                'name' => 'Test project name',
+                'description' => 'Test project description',
+            ])
+            ->call('store');
+
+        $this->assertTrue(Project::where('name', 'Test project name')->exists());
+    }
+
+    public function test_valid_input_must_be_provided_before_project_can_be_created(): void
+    {
+        $this->actingAs($user = User::factory()->withPersonalTeam()->create());
+
+        Livewire::test(Projects::class, ['team' => $user->fresh()->personalTeam()->id])
+            ->set([
+                'name' => '',
+                'description' => '',
+            ])
+            ->call('store')
+            ->assertHasErrors(['name', 'description']);
+    }
+}
