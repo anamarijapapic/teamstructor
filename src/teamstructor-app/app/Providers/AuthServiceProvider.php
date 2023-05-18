f5e956f5f5b2775baa69dc405eed4ca44246b120
Anamarija Papic <anamarija.papic@agilo.co>
2023-05-18 08:51:33 +0200

    Register policies

--- src/teamstructor-app/app/Providers/AuthServiceProvider.php
+++ src/teamstructor-app/app/Providers/AuthServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Resource;
 use App\Models\Team;
 use App\Models\User;
+use App\Policies\CommentPolicy;
+use App\Policies\PostPolicy;
 use App\Policies\ProjectPolicy;
+use App\Policies\ResourcePolicy;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -19,6 +25,9 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Team::class => TeamPolicy::class,
         Project::class => ProjectPolicy::class,
+        Post::class => PostPolicy::class,
+        Comment::class => CommentPolicy::class,
+        Resource::class => ResourcePolicy::class,
     ];
 
     /**
