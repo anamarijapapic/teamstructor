6892f5051f500512f28271b021c2715d94500a2a
Anamarija Papic <anamarija.papic@agilo.co>
2023-05-18 15:52:12 +0200

    App requires authentication (log in to view)

--- src/teamstructor-app/resources/views/auth/login.blade.php
+++ src/teamstructor-app/resources/views/auth/login.blade.php
@@ -17,12 +17,14 @@
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                    autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -34,7 +36,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
@@ -42,6 +45,15 @@
                 <x-button class="ml-4">
                     {{ __('Log in') }}
                 </x-button>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}"
+                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('Not a member? Register now!') }}
+                    </a>
+                @endif
             </div>
         </form>
     </x-authentication-card>
--- src/teamstructor-app/routes/web.php
+++ src/teamstructor-app/routes/web.php
@@ -14,30 +14,32 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+ */
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return view('dashboard');
     })->name('dashboard');
 
-    Route::get('/teams/{team}/projects', ShowProjects::class)->name('teams.projects');
-    Route::get('/teams/{team}/projects/{project}/discussion', ShowPosts::class)->name('teams.projects.discussion');
-    Route::get('/teams/{team}/projects/{project}/resources', ShowResources::class)->name('teams.projects.resources');
+    Route::get('teams/{team}/projects', ShowProjects::class)
+        ->name('teams.projects');
+
+    Route::get('teams/{team}/projects/{project}/discussion', ShowPosts::class)
+        ->name('teams.projects.discussion');
+
+    Route::get('teams/{team}/projects/{project}/resources', ShowResources::class)
+        ->name('teams.projects.resources');
 });
 
-Route::middleware([
-    'can:admin-privileges',
-])->group(function () {
-    Route::get('/admin-dashboard', function () {
+Route::group([
+    'prefix' => 'admin',
+    'middleware' => ['auth:sanctum', 'can:admin-privileges'],
+], function () {
+    Route::get('/', function () {
         return view('admin-dashboard');
-    })->name('admin-dashboard');
+    })->name('admin.dashboard');
 });
